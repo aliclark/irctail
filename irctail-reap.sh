335c5a9bd51a84a794c739180bde1da7a69875bc
Ali Clark <ali@clark.gb.net>
2013-02-12 23:23:18 +0000

    only grep tail instead of tailf, more permissive

--- irctail-reap.sh
+++ irctail-reap.sh
@@ -6,6 +6,6 @@ if [ "$1" == 'all' ]; then
     thisdate='oajfoj3983a3' # reap everything
 fi
 
-for pid in `ps ux | grep 'tailf' | grep -v $thisdate | grep log | awk '{ print $2 }'`; do
+for pid in `ps ux | grep 'tail' | grep -v $thisdate | grep log | awk '{ print $2 }'`; do
     kill $pid
 done
