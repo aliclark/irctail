2b1a75dad299650d2500577da0edc4259a5e3e6b
Ali Clark <ali@clark.gb.net>
2013-02-12 23:58:25 +0000

    more renaming to indicate scripts are irc client agnostic

--- base.aclark.theme.py
+++ base.aclark.theme.py
@@ -4,4 +4,4 @@ if [ ! -e /tmp/irctail ]; then
     touch /tmp/irctail
 fi
 
-tailf /tmp/irctail | ./irssi-chanformat.py | ./irssi-chanshift.py | ./base-nameformat.py | ./irc-timeformat.py | ./irc-colorize.py >>/tmp/feed
+tailf /tmp/irctail | ./irc-chanformat.py | ./irc-chanshift.py | ./base-nameformat.py | ./irc-timeformat.py | ./irc-colorize.py >>/tmp/feed
