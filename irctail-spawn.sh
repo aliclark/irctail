7c7192127ec6de0d8fd34349c11c5a30b058dfae
Ali Clark <ali.clark@openmarket.com>
2013-02-12 09:47:51 +0000

    grep for tail not tailf when checking if process exists

--- irctail-spawn.sh
+++ irctail-spawn.sh
@@ -8,7 +8,7 @@ basestr=`date "+$dirformat"`
 
 for x in `ls $basestr`; do
     if [ "$onlythese" == '' ] || [[ "$onlythese" == *"$x"* ]]; then
-        if [ -e $basestr/$x ] && ! ps ux | grep tailf | grep $basestr/$x >/dev/null ; then
+        if [ -e $basestr/$x ] && ! ps ux | grep tail | grep $basestr/$x >/dev/null ; then
             tailf $basestr/$x | sed -u "s/^/`echo $x | sed 's/\.log//'`\t/" >>$outfile &
         fi
     fi
