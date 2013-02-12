8e9dc790428b8f3821df073675ea027b178fa8cc
Ali Clark <ali@clark.gb.net>
2013-02-12 23:00:13 +0000

    fix base-nameformat to understand /me and /part style formatting

--- base-nameformat.py
+++ base-nameformat.py
@@ -14,24 +14,26 @@ allocated  = {}
 
 textmatcher = {}
 
-def gotname(before, name, after):
+def gotname(before, name, after, leadstr=None):
     global maxlen
     namet = name
-    maxlen = il.getmaxlen(name, maxlen)
+    if not leadstr:
+        leadstr = ''
+    maxlen = il.getmaxlen(leadstr + name, maxlen)
     color = il.getcolor(namet, allocated, counts, hits)
     il.uplogs(color, hits)
     after = il.text_colorize(after, textmatcher, allocated)
-    return before + il.getmaxpad(name, maxlen) + color + name + il.clearseq + after
+    return before + il.getmaxpad(leadstr + name, maxlen) + leadstr + color + name + il.clearseq + after
 
 def main():
     # try to match two columns and a 3rd name column
-    namematcher = re.compile('^(\ *[^\ \t]+[\ \t]+[^\ \t]+)[\ \t]([^\w]*[^\ \t]+)[\ \t](.*)')
+    namematcher = re.compile('^(\ *[^\ \t]+[\ \t]+[^\ \t]+)[\ \t]((---|\*)\ )?([^\w]*[^\ \t]+)[\ \t](.*)')
     try:
         line = sys.stdin.readline()
         while line:
             result = namematcher.match(line)
             if result:
-                line = gotname(result.group(1) + ' ', result.group(2), ' ' + result.group(3) + '\n')
+                line = gotname(result.group(1) + ' ', result.group(4), ' ' + result.group(5) + '\n', result.group(2))
 
             print(line, end='')
             sys.stdout.flush()
