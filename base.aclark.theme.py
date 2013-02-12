0041a0f07bdbebd699e7199741c989d3f2178f01
Ali Clark <ali@clark.gb.net>
2013-02-12 23:55:51 +0000

    renam irssi-colorize.py -> irc-colorize.py

--- base.aclark.theme.py
+++ base.aclark.theme.py
@@ -4,4 +4,4 @@ if [ ! -e /tmp/irctail ]; then
     touch /tmp/irctail
 fi
 
-tailf /tmp/irctail | ./irssi-chanformat.py | ./irssi-chanshift.py | ./base-nameformat.py | ./irc-timeformat.py | ./irssi-colorize.py >>/tmp/feed
+tailf /tmp/irctail | ./irssi-chanformat.py | ./irssi-chanshift.py | ./base-nameformat.py | ./irc-timeformat.py | ./irc-colorize.py >>/tmp/feed
--- irssi.aclark.theme.py
+++ irssi.aclark.theme.py
@@ -4,4 +4,4 @@ if [ ! -e /tmp/irssitail ]; then
     touch /tmp/irssitail
 fi
 
-tailf /tmp/irssitail | ./irssi-chanformat.py | ./irssi-chanshift.py | ./irssi-nameformat.py | ./irssi-colorize.py >>/tmp/feed
+tailf /tmp/irssitail | ./irssi-chanformat.py | ./irssi-chanshift.py | ./irssi-nameformat.py | ./irc-colorize.py >>/tmp/feed
