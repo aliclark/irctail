63339eff5cefb23be21e19eff8ad7f926d7770c0
Ali Clark <ali.clark@openmarket.com>
2013-02-13 10:59:52 +0000

    echo the log file being tailed

--- irctail-spawn.sh
+++ irctail-spawn.sh
@@ -9,6 +9,7 @@ basestr=`date "+$dirformat"`
 for x in `ls $basestr`; do
     if [ "$onlythese" == '' ] || [[ "$onlythese" == *"$x"* ]]; then
         if [ -e $basestr/$x ] && ! ps ux | grep tail | grep -E "$basestr/$x\$" >/dev/null ; then
+            echo "$basestr/$x"
             tailf $basestr/$x | sed -u "s/^/`echo $x | sed 's/\.log//'`\t/" >>$outfile &
         fi
     fi
