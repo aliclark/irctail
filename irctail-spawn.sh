b8c96f00527515aec7207f19b6d1ebdcd5b59a08
Ali Clark <ali.clark@openmarket.com>
2013-02-13 11:00:44 +0000

    comment about multiple ps ux calls

--- irctail-spawn.sh
+++ irctail-spawn.sh
@@ -6,6 +6,8 @@ onlythese=''
 
 basestr=`date "+$dirformat"`
 
+# TODO: save results of ps aux | grep tail somewhere!
+
 for x in `ls $basestr`; do
     if [ "$onlythese" == '' ] || [[ "$onlythese" == *"$x"* ]]; then
         if [ -e $basestr/$x ] && ! ps ux | grep tail | grep -E "$basestr/$x\$" >/dev/null ; then
