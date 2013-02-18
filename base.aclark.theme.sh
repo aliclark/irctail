1dc05705e39626e46bd0d6c43ff7e2322532c4f9
Ali Clark <ali.clark@openmarket.com>
2013-02-18 12:38:14 +0000

    make base.aclark.theme.sh a simple pipe, unaware of source file

--- base.aclark.theme.sh
+++ base.aclark.theme.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-if [ ! -e /tmp/irctail ]; then
-    touch /tmp/irctail
-fi
-
-tailf /tmp/irctail | ./irc-chanformat.py | ./irc-chanshift.py | ./base-nameformat.py | ./base-timeformat.py | ./irc-colorize.py
+./irc-chanformat.py | ./irc-chanshift.py | ./base-nameformat.py | ./base-timeformat.py | ./irc-colorize.py
