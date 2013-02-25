5914148f696b3ab7d34820d8974c12e154657d96
Ali Clark <ali.clark@openmarket.com>
2013-02-25 13:42:37 +0000

    make irssi.aclark.theme.py a simple pipe like base.aclark.theme.py

--- irssi.aclark.theme.py
+++ irssi.aclark.theme.py
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-if [ ! -e /tmp/irssitail ]; then
-    touch /tmp/irssitail
-fi
-
-tailf /tmp/irssitail | ./irc-chanformat.py | ./irc-chanshift.py | ./irssi-nameformat.py | ./irc-colorize.py >>/tmp/feed
+./irc-chanformat.py | ./irc-chanshift.py | ./irssi-nameformat.py | ./irc-colorize.py
