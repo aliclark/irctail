8f53554890b6cd9224f302b4ec9798c12f4cad2e
Ali Clark <ali.clark@openmarket.com>
2013-05-13 15:20:18 +0100

    fix baseformat to allow empty text content

--- baseformat.py
+++ baseformat.py
@@ -93,7 +93,7 @@ def combine_parts(channel, time, name, text):
         channel = ''
     return time + tcsep + channel + ' ' + name + ' ' + text
 
-splitter = re.compile(r'^(([^\t]+)\t)?([^\t]+)\t([^\t]+)\t(.+)$')
+splitter = re.compile(r'^(([^\t]+)\t)?([^\t]+)\t([^\t]+)\t(.*)$')
 
 def main():
     try:
